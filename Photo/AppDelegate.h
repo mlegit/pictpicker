94fad1441fa8e5cd2799b01160c427abd384c589
Emilie Noel <noele@Emilies-iMac.local>
2012-05-20 08:06:24 -0400

    Initial Commit

---
+++ Photo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo
+//
+//  Created by Emilie Noel on 12-05-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoTests/PhotoTests.m
@@ -0,0 +1,32 @@
+//
+//  PhotoTests.m
+//  PhotoTests
+//
+//  Created by Emilie Noel on 12-05-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PhotoTests.h"
+
+@implementation PhotoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PhotoTests");
+}
+
+@end
